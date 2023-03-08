cfc28eb7dc777f5d77931b98ab0466da04d3e739
prekshahirani <96530274+prekshahirani@users.noreply.github.com>
2023-03-08 15:26:23 +0530

    Update admin.py

--- Lab_1/MyApp/admin.py
+++ Lab_1/MyApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from MyApp.models import User
+from MyApp.models import Administrator
+from MyApp.models import Content
+from MyApp.models import like_content
+from MyApp.models import Image
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Administrator)
+admin.site.register(Content)
+admin.site.register(like_content)
+admin.site.register(Image)
